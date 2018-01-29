cb0cf99819bb56b81d135e84498d7d28fbd55154
Armon Dadgar <armon.dadgar@gmail.com>
2018-01-29 10:00:36 -0800

    Starting work on DB backend

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ test:
 build:
 	go build -o bin/counterd ./counterd/
 
+pg:
+	docker run -p 5432:5432 -d postgres:9.6
+
+psql:
+	psql -h 127.0.0.1 -p 5432 -U postgres
+
--- counterd/db.go
+++ counterd/db.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+)
 
 // DatabaseClient is used to abstract the DB for testing
 type DatabaseClient interface {
@@ -10,3 +16,67 @@ type DatabaseClient interface {
 	// UpsertCounter is used to register the counter value, updating if it exists
 	UpsertCounter(interval string, date time.Time, attributes map[string]string, count int64) error
 }
+
+// PGDatabase provides a database client backed by PostgreSQL
+type PGDatabase struct {
+	db *sql.DB
+
+	// Prepared queries we store
+	upsertDomain  *sql.Stmt
+	upsertCounter *sql.Stmt
+}
+
+// NewPGDatabase creates a PGDatabase connection with a URL string
+// "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
+func NewPGDatabase(connStr string) (*PGDatabase, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Setup the DB connection
+	pg := &PGDatabase{
+		db: db,
+	}
+
+	// Create the prepared queries
+	stmt, err := db.Prepare(upsertDomainSQL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepared query: %v", err)
+	}
+	pg.upsertDomain = stmt
+
+	stmt, err = db.Prepare(upsertCounterSQL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepared query: %v", err)
+	}
+	pg.upsertCounter = stmt
+
+	return pg, nil
+}
+
+const (
+	// upsertDomainSQL is used to upsert values into the domain table
+	upsertDomainSQL = `INSERT INTO attributes_domain VALUES (?, ?) ON CONFLICT DO NOTHING;`
+
+	// upsertCounterSQL is used to upsert into the counters table
+	upsertCounterSQL = `INSERT INTO counters (interval, date, attributes, count) VALUES (?, ?, ?, ?) ON CONFLICT (interval, date, attributes) DO UPDATE SET count = GREATEST(EXCLUDED.count, counters.count);`
+
+	// createDomainSQL is used to create the domain table
+	createDomainSQL = `CREATE TABLE IF NOT EXISTS attributes_domain (
+		attribute text NOT NULL,
+		value text NOT NULL,
+		PRIMARY KEY (attribute, value)
+	);`
+
+	// createCounterSQL is used to create the counter table
+	createCounterSQL = `CREATE TABLE IF NOT EXISTS counters (
+	    id uuid DEFAULT uuid_generate_v4(),
+		interval varchar(16) NOT NULL,
+		date timestamp NOT NULL,
+		attributes jsonb NOT NULL,
+		count bigint DEFAULT 0,
+		PRIMARY KEY (id),
+		UNIQUE (interval, date, attributes)
+	);`
+)
